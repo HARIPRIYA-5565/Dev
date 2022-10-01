cd08fe6fd5c1d209e9e95258cf08d4b7419477e3
HARIPRIYA-5565 <70848158+HARIPRIYA-5565@users.noreply.github.com>
2022-10-01 13:23:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import FormPractice from "../Components/FormPractice"
 export default function Home() {
   return (
     <div>
+    <h1> For further process please proceed with the form</h1>
       <FormPractice/>
     </div>
    
